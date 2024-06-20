48a727c38d8ca04517fb5ab7441f36e31672f3c5
AthariqByan <fadhlansantoso25@gmail.com>
2024-06-20 20:35:33 +0700

    add view login & register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,7 +61,7 @@
                                 <div class="card-body">
 
                                     <div class="pt-4 pb-2">
-                                        <h5 class="card-title text-center pb-0 fs-4">Login</h5>
+                                        <h5 class="card-title text-center pb-0 fs-4">Masuk</h5>
                                     </div>
 
                                     <form class="row g-3 needs-validation" novalidate>
